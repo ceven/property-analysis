e8c2167635dc4230ac760a0c4475da6fcd9be92c
Caroline Even <carolineven29@gmail.com>
2019-08-09 05:27:18 +1000

    Update view to display all graphs

--- src/web/app/views.py
+++ src/web/app/views.py
@@ -8,8 +8,8 @@ from . import models
 # Create your views here.
 def index(request):
     p_data, r_data = models.get_property_data()
-    graphic = charts.get_chart_graphic(p_data[0], r_data[0])
-    context = {'graphic': graphic, 'home_name': p_data[0].home_name}  # FIXME find a way to pass all properties to view
+    graphs = [charts.get_chart_graphic(p, r) for (p, r) in zip(p_data, r_data)]
+    context = {'graphs': graphs, 'homes': p_data}
     return render(request, 'index.html', context=context)  # TODO try to render image as svg instead of png
 
 
